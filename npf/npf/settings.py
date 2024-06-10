6d21e690a3f303cb988f5954c0951195cc2b1f5b
vishaldhakal <vishaldhakal96@gmail.com>
2024-06-10 15:39:17 +0545

    dfv

--- npf/npf/settings.py
+++ npf/npf/settings.py
@@ -72,23 +72,23 @@ WSGI_APPLICATION = "npf.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}
+} """
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'npf',
-#         'USER': 'npf_damin',
-#         'PASSWORD': 'npfadminpassword',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'npf',
+        'USER': 'npf_damin',
+        'PASSWORD': 'npfadminpassword',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
