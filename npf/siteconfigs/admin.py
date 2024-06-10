d5aedabb19e8554cbee5c74bdc1b15bc2dd067ac
Manav Khadka <manavkhadka0@gmail.com>
2024-06-10 15:34:00 +0545

    dsada

--- npf/siteconfigs/admin.py
+++ npf/siteconfigs/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import DonationContent
 from django import forms
 from tinymce.widgets import TinyMCE
+from solo.admin import SingletonModelAdmin
 from unfold.admin import ModelAdmin
 
 
@@ -15,3 +16,13 @@ class DonationContentForm(forms.ModelForm):
         }
 
 
+class CustomDonationContentAdmin(SingletonModelAdmin, ModelAdmin):
+    form = DonationContentForm
+
+    # If there are any conflicts in method resolution order, ensure SingletonModelAdmin methods are prioritized
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+        SingletonModelAdmin.__init__(self, model, admin_site)
+
+
+admin.site.register(DonationContent, CustomDonationContentAdmin)
