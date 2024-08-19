40a22c910fe6d43a2c326de2056f0912dfa87b77
Manav Khadka <manavkhadka0@gmail.com>
2024-08-19 13:27:05 +0545

    Refactor database configuration in settings.py

--- npf/npf/settings.py
+++ npf/npf/settings.py
@@ -72,14 +72,14 @@ WSGI_APPLICATION = "npf.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}
+} """
 
-""" DATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "npf",
@@ -88,7 +88,7 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "",
     }
-} """
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
